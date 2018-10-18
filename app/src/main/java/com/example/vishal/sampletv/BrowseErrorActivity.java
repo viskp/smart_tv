35a877107f43001973bb69c085d923f666eb6709
viskp <viskp@live.in>
2018-10-18 12:04:10 +0530

    two again

--- app/src/main/java/com/example/vishal/sampletv/BrowseErrorActivity.java
+++ app/src/main/java/com/example/vishal/sampletv/BrowseErrorActivity.java
@@ -47,6 +47,7 @@ public class BrowseErrorActivity extends Activity {
 
     private void testError() {
         //two
+        //two again
         mErrorFragment = new ErrorFragment();
         getFragmentManager().beginTransaction().add(R.id.main_browse_fragment, mErrorFragment).commit();
 
